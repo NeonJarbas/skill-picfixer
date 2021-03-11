e994ea4a568a662c3a6cbaf5ebfdd8c4df63fb3a
jarbasal <jarbasai@mailfence.com>
2021-03-11 23:38:34 +0000

    better_cps

---
+++ __init__.py
@@ -0,0 +1,72 @@
+from ovos_utils.skills.templates.video_collection import VideoCollectionSkill
+from mycroft.skills.core import intent_file_handler
+from pyvod import Collection, Media
+from os.path import join, dirname, basename
+from ovos_utils.playback import CPSMatchType, CPSPlayback, CPSMatchConfidence
+
+
+class PicFixerSkill(VideoCollectionSkill):
+
+    def __init__(self):
+        super().__init__("PicFixer")
+        self.supported_media = [CPSMatchType.GENERIC,
+                                CPSMatchType.MOVIE,
+                                CPSMatchType.VIDEO]
+        self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
+        # load video catalog
+        path = join(dirname(__file__), "res", "feature_films_picfixer.jsondb")
+        logo = join(dirname(__file__), "res", "picfixer.png")
+        self.media_collection = Collection("feature_films_picfixer", logo=logo, db_path=path)
+        self.default_image = join(dirname(__file__), "ui", "picfixer.png")
+        self.skill_logo = join(dirname(__file__), "ui", "picfixer.png")
+        self.skill_icon = join(dirname(__file__), "ui", "picfixer.png")
+        self.default_bg = logo
+        self.media_type = CPSMatchType.MOVIE
+        self.playback_type = CPSPlayback.GUI
+
+    # voice interaction
+    def get_intro_message(self):
+        self.speak_dialog("intro")
+
+    @intent_file_handler('home.intent')
+    def handle_homescreen_utterance(self, message):
+        self.handle_homescreen(message)
+
+    # better common play
+    def normalize_title(self, title):
+        title = title.lower().strip()
+        title = self.remove_voc(title, "picfixer")
+        title = self.remove_voc(title, "movie")
+        title = self.remove_voc(title, "play")
+        title = self.remove_voc(title, "video")
+        title = self.remove_voc(title, "scifi")
+        title = self.remove_voc(title, "short")
+        title = self.remove_voc(title, "horror")
+        title = title.replace("|", "").replace('"', "") \
+            .replace(':', "").replace('”', "").replace('“', "") \
+            .strip()
+        return " ".join([w for w in title.split(" ") if w])  # remove extra spaces
+
+    def match_media_type(self, phrase, media_type):
+        score = 0
+        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+            score += 5
+
+        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+            score += 10
+
+        if self.voc_match(phrase, "old"):
+            score += 10
+
+        if self.voc_match(phrase, "public_domain"):
+            score += 15
+
+        if self.voc_match(phrase, "picfixer"):
+            score += 50
+
+        return score
+
+
+def create_skill():
+    return PicFixerSkill()
+
---
+++ readme.md
@@ -0,0 +1,34 @@
+# <img src='./ui/picfixer.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/>Picfixer Feature Film Collection
+
+
+## About
+
+COMEDY, DRAMA, ADVENTURE, MYSTERY, ROMANCE, SCI-FI, HORROR, WESTERNS, DOCUMENTARIES, EXPLOITATION. NOSTALGIC DOUBLE-FEATURE THEATER PROGRAMS You will find them all in this free movie loft!
+
+
+![](./gui.png)
+![](./gui2.png)
+
+## Installation Notes
+
+this requires [skill-better-playback-control](https://github.com/JarbasSkills/skill-better-playback-control), it will blacklist and replace the official mycroft skill
+
+## Examples
+
+* "open pic fixer"
+* "play old movie"
+* "play public domain movie"
+
+## Credits
+- JarbasAl
+- [AIIX](https://github.com/AIIX/) - GUI
+- [PicFixer Feature Films](https://archive.org/details/feature_films_picfixer)
+
+## Category
+**Entertainment**
+
+## Tags
+#feature-films
+#movies
+#video
+#entertainment
