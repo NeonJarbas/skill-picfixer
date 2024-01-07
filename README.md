95b9fb1ee46a93e222ea94165194590a7bcbc335
JarbasAi <jarbasai@mailfence.com>
2024-01-07 06:12:29 +0000

    refactor/ocp

---
+++ README.md
@@ -0,0 +1,34 @@
+# <img src='./ui/picfixer.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/>Picfixer Feature Film Collection
+
+## About
+
+COMEDY, DRAMA, ADVENTURE, MYSTERY, ROMANCE, SCI-FI, HORROR, WESTERNS, DOCUMENTARIES, EXPLOITATION. NOSTALGIC
+DOUBLE-FEATURE THEATER PROGRAMS You will find them all in this free movie loft!
+
+![](./gui.png)
+![](./gui2.png)
+
+
+## Examples
+
+* "open pic fixer"
+* "play old movie"
+* "play public domain movie"
+
+## Credits
+
+- JarbasAl
+- [AIIX](https://github.com/AIIX/) - GUI
+- [PicFixer Feature Films](https://archive.org/details/feature_films_picfixer)
+
+## Category
+
+**Entertainment**
+
+## Tags
+
+#feature-films
+#movies
+#video
+#public-domain
+#entertainment
--- __init__.py
+++ __init__.py
@@ -1,72 +1,144 @@
-from os.path import join, dirname, basename
+from os.path import join, dirname
 
-from mycroft.skills.core import intent_file_handler
-from ovos_plugin_common_play.ocp import MediaType, PlaybackType
-from ovos_workshop.skills.video_collection import VideoCollectionSkill
-from pyvod import Collection
+from json_database import JsonStorage
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 
 
-class PicFixerSkill(VideoCollectionSkill):
-
-    def __init__(self):
-        super().__init__("PicFixer")
-        self.supported_media = [MediaType.MOVIE]
-        self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
+class PicFixerSkill(OVOSCommonPlaybackSkill):
+    def __init__(self, *args, **kwargs):
+        self.supported_media = [MediaType.MOVIE, MediaType.BLACK_WHITE_MOVIE]
         # load video catalog
         path = join(dirname(__file__), "res", "feature_films_picfixer.jsondb")
         logo = join(dirname(__file__), "res", "picfixer.png")
-        self.media_collection = Collection("feature_films_picfixer", logo=logo,
-                                           db_path=path)
+        self.archive = {v["streams"][0]: v for v in JsonStorage(path)["feature_films_picfixer"] if v["streams"]}
+
         self.default_image = join(dirname(__file__), "ui", "picfixer.png")
         self.skill_logo = join(dirname(__file__), "ui", "picfixer.png")
         self.skill_icon = join(dirname(__file__), "ui", "picfixer.png")
         self.default_bg = logo
-        self.media_type = MediaType.MOVIE
-        self.playback_type = PlaybackType.VIDEO
+        super().__init__(*args, **kwargs)
+        self.load_ocp_keywords()
 
-    # voice interaction
-    def get_intro_message(self):
-        self.speak_dialog("intro")
+    def load_ocp_keywords(self):
+        title = []
+        bw_movies = []
+        silent_movies = []
+        docus = []
 
-    @intent_file_handler('home.intent')
-    def handle_homescreen_utterance(self, message):
-        self.handle_homescreen(message)
+        for url, data in self.archive.items():
+            t = data["title"].split("|")[0].split("(")[0].split(" video")[0].split(" restored")[0].strip()
+            if "documentaries" in [_.lower() for _ in data["tags"]]:
+                t = t
+                docus.append(t)
+            elif data.get("sound") in ["silent", "Silent, No Music"]:
+                silent_movies.append(t)
+            elif data.get("color") in ["b&w", "black & white", "black and white", "B&W"]:
+                bw_movies.append(t)
+                if ":" in t:
+                    t1, t2 = t.split(":", 1)
+                    bw_movies.append(t1.strip())
+                    bw_movies.append(t2.strip())
+            else:
+                title.append(t)
+                if ":" in t:
+                    t1, t2 = t.split(":", 1)
+                    title.append(t1.strip())
+                    title.append(t2.strip())
 
-    # better common play
-    def normalize_title(self, title):
-        title = title.lower().strip()
-        title = self.remove_voc(title, "picfixer")
-        title = self.remove_voc(title, "movie")
-        title = self.remove_voc(title, "play")
-        title = self.remove_voc(title, "video")
-        title = self.remove_voc(title, "scifi")
-        title = self.remove_voc(title, "short")
-        title = self.remove_voc(title, "horror")
-        title = title.replace("|", "").replace('"', "") \
-            .replace(':', "").replace('”', "").replace('“', "") \
-            .strip()
-        return " ".join(
-            [w for w in title.split(" ") if w])  # remove extra spaces
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "movie_name", title)
+        self.register_ocp_keyword(MediaType.BLACK_WHITE_MOVIE,
+                                  "bw_movie_name", bw_movies)
+        self.register_ocp_keyword(MediaType.SILENT_MOVIE,
+                                  "silent_movie_name", silent_movies)
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_name", docus)
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "movie_streaming_provider",
+                                  ["PicFixer",
+                                   "Picfixer Feature Film Collection",
+                                   "Pic Fixer"])
 
-    def match_media_type(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "video") or media_type == MediaType.VIDEO:
-            score += 5
+    def get_playlist(self, score=50, num_entries=25):
+        pl = self.featured_media()[:num_entries]
+        return {
+            "match_confidence": score,
+            "media_type": MediaType.MOVIE,
+            "playlist": pl,
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "image": self.default_image,
+            "title": "Picfixer Feature Film Collection (Movie Playlist)",
+            "author": "PicFixer"
+        }
 
-        if self.voc_match(phrase, "movie") or media_type == MediaType.MOVIE:
-            score += 10
+    @ocp_search()
+    def search_db(self, phrase, media_type):
+        base_score = 15 if media_type in [MediaType.MOVIE, MediaType.BLACK_WHITE_MOVIE] else 0
+        entities = self.ocp_voc_match(phrase)
 
-        if self.voc_match(phrase, "old"):
-            score += 10
+        title = entities.get("movie_name")
+        bw_title = entities.get("bw_movie_name")
+        print(bw_title)
+        s_title = entities.get("silent_movie_name")
+        skill = "movie_streaming_provider" in entities  # skill matched
 
-        if self.voc_match(phrase, "public_domain"):
-            score += 15
+        base_score += 30 * len(entities)
 
-        if self.voc_match(phrase, "picfixer"):
-            score += 50
+        if title or bw_title or s_title:
+            candidates = self.archive.values()
+            media_type = MediaType.MOVIE
+            if title:
+                base_score += 20
+                candidates = [video for video in self.archive.values()
+                              if title.lower() in video["title"].lower()]
+            elif bw_title:
+                media_type = MediaType.BLACK_WHITE_MOVIE
+                base_score += 10
+                candidates = [video for video in self.archive.values()
+                              if bw_title.lower() in video["title"].lower()]
+            elif s_title:
+                media_type = MediaType.SILENT_MOVIE
+                base_score += 10
+                candidates = [video for video in self.archive.values()
+                              if s_title.lower() in video["title"].lower()]
 
-        return score
+            for video in candidates:
+                yield {
+                    "title": video["title"],
+                    "match_confidence": min(100, base_score),
+                    "media_type": media_type,
+                    "uri": video["streams"][0],
+                    "playback": PlaybackType.VIDEO,
+                    "skill_icon": self.skill_icon,
+                    "skill_id": self.skill_id,
+                    "image": self.default_image
+                }
+
+        if skill:
+            yield self.get_playlist()
+
+    @ocp_featured_media()
+    def featured_media(self):
+        return [{
+            "title": video["title"],
+            "match_confidence": 70,
+            "media_type": MediaType.MOVIE,
+            "uri": video["streams"][0],
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "bg_image": self.default_bg,
+            "skill_id": self.skill_id
+        } for video in self.archive.values()]
 
 
-def create_skill():
-    return PicFixerSkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
+
+    s = PicFixerSkill(bus=FakeBus(), skill_id="t.fake")
+    for r in s.search_db("play TEENAGE ZOMBIES", MediaType.BLACK_WHITE_MOVIE):
+        print(r)
+        # {'title': 'TEENAGE ZOMBIES video quality upgrade', 'match_confidence': 55, 'media_type': <MediaType.BLACK_WHITE_MOVIE: 20>, 'uri': 'https://archive.org/download/TeenageZombiesVideoQualityUpgrade/TeenageZombiesVideoQualityUpgrade.ogv', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake', 'image': '/home/miro/PycharmProjects/OCP_sprint/skills/skill-picfixer/ui/picfixer.png'}
